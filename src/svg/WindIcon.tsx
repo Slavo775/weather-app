347e36ece16f53c1243e8bdad7cf81fc8fdff6df
Slavomir Sedlak <slavomir.sedlak@justeattakeaway.com>
2024-09-13 08:42:31 +0200

    feat: create WeatherDetailCard component and Weather DetailWrapper component and use it.

---
+++ src/svg/WindIcon.tsx
@@ -0,0 +1,53 @@
+export default function WindIcon () {
+    return (<svg width="33" height="27" viewBox="0 0 33 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_ddi_4_73)">
+                <path
+                    d="M6.66055 11.8716C6.3059 11.8716 6.01835 12.1591 6.01835 12.5138C6.01835 12.8685 6.3059 13.156 6.66055 13.156H21.7749C21.5563 13.5338 21.4312 13.9725 21.4312 14.4404C21.4312 15.8591 22.5813 17.0092 24 17.0092C25.4187 17.0092 26.5688 15.8591 26.5688 14.4404C26.5688 13.0216 25.4187 11.8716 24 11.8716H6.66055Z"
+                    fill="#BD00FF"/>
+                <path
+                    d="M20.6324 9.84955C20.545 10.1903 20.7509 10.5429 21.1014 10.573C21.7343 10.6274 22.3744 10.5247 22.9633 10.2695C23.7481 9.92936 24.3975 9.33778 24.8091 8.58795C25.2206 7.83811 25.371 6.97265 25.2365 6.12794C25.1019 5.28323 24.6902 4.5073 24.066 3.9224C23.4419 3.3375 22.6409 2.9769 21.7893 2.89742C20.9376 2.81793 20.0837 3.02408 19.3621 3.48337C18.6406 3.94267 18.0923 4.62901 17.8038 5.43424C17.5873 6.03846 17.5263 6.68385 17.6216 7.31193C17.6743 7.65967 18.0396 7.84228 18.3739 7.73303C18.7082 7.62377 18.882 7.26258 18.8578 6.91169C18.8334 6.55839 18.8819 6.20152 19.0028 5.86386C19.196 5.3248 19.563 4.86533 20.0461 4.55786C20.5291 4.25038 21.1008 4.11237 21.6709 4.16559C22.241 4.2188 22.7773 4.4602 23.1951 4.85176C23.6129 5.24332 23.8886 5.76277 23.9786 6.32826C24.0687 6.89375 23.968 7.47313 23.6925 7.97511C23.417 8.47708 22.9823 8.87312 22.4569 9.1008C22.1278 9.24342 21.7748 9.31491 21.4206 9.31346C21.0689 9.31203 20.7197 9.50886 20.6324 9.84955Z"
+                    fill="#BD00FF"/>
+                <path
+                    d="M8.58716 9.94496C8.58716 9.59028 8.87468 9.30276 9.22936 9.30276H21.4312V10.5872H9.22936C8.87468 10.5872 8.58716 10.2996 8.58716 9.94496Z"
+                    fill="#BD00FF"/>
+                <path
+                    d="M9.87156 15.0826C9.87156 14.7279 10.1591 14.4404 10.5138 14.4404H16.9358C17.2905 14.4404 17.578 14.7279 17.578 15.0826C17.578 15.4373 17.2905 15.7248 16.9358 15.7248H10.5138C10.1591 15.7248 9.87156 15.4373 9.87156 15.0826Z"
+                    fill="#BD00FF"/>
+                <path
+                    d="M14.367 8.01835C15.431 8.01835 16.2936 7.15579 16.2936 6.09175C16.2936 5.02771 15.431 4.16514 14.367 4.16514C13.3029 4.16514 12.4404 5.02771 12.4404 6.09175C12.4404 6.31693 12.4789 6.53306 12.55 6.73395H6.66055C6.3059 6.73395 6.01835 7.02146 6.01835 7.37615C6.01835 7.73084 6.3059 8.01835 6.66055 8.01835H14.367Z"
+                    fill="#BD00FF"/>
+            </g>
+            <defs>
+                <filter id="filter0_ddi_4_73" x="-1.68807" y="0.31193" width="35.9633" height="29.5413"
+                        filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                   result="hardAlpha"/>
+                    <feOffset/>
+                    <feGaussianBlur stdDeviation="1.2844"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix"
+                                   values="0 0 0 0 0.717647 0 0 0 0 0.0941176 0 0 0 0 0.203922 0 0 0 0.4 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_73"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                   result="hardAlpha"/>
+                    <feOffset dy="5.13761"/>
+                    <feGaussianBlur stdDeviation="3.85321"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.741176 0 0 0 0 0 0 0 0 0 1 0 0 0 0.38 0"/>
+                    <feBlend mode="normal" in2="effect1_dropShadow_4_73" result="effect2_dropShadow_4_73"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_4_73" result="shape"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                   result="hardAlpha"/>
+                    <feOffset dx="-1.2844" dy="-1.2844"/>
+                    <feGaussianBlur stdDeviation="0.642202"/>
+                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="shape" result="effect3_innerShadow_4_73"/>
+                </filter>
+            </defs>
+        </svg>
+
+
+    )
+}
