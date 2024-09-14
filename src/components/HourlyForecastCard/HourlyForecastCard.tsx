999e58244eece423984b0a4714dd6ab9282ca819
Slavomir Sedlak <slavomir.sedlak@justeattakeaway.com>
2024-09-14 14:18:51 +0200

    feat: add pure-react-carousel and use it for creating hourly forecast

--- src/components/HourlyForecastCard/HourlyForecastCard.tsx
+++ src/components/HourlyForecastCard/HourlyForecastCard.tsx
@@ -1,8 +1,16 @@
 import './HourlyForecastCard.scss'
 
-export default function HourlyForecastCard({imgSrc, time, temp, textState, isActive, key}: {imgSrc: string, time: string, temp: number, textState: string, isActive: boolean, key: string}) {
+type ComponentProps = {
+    imgSrc: string,
+    time: string,
+    temp: number,
+    textState: string,
+    isActive: boolean
+}
+
+export default function HourlyForecastCard({imgSrc, time, temp, textState, isActive}: ComponentProps) {
     return (
-        <div className={`hourly-forecast-card ${isActive && 'hourly-forecast-card--active'}`} key={key}>
+        <div className={`hourly-forecast-card ${isActive && 'hourly-forecast-card--active'}`}>
             <span className={'hourly-forecast-card__time'}>{ !isActive ? time : 'Teraz'}</span>
             <img src={imgSrc} className={'hourly-forecast-card__icon'} alt={`Hodinova predpoved pocasia ${time} ${textState}`}/>
             <span className={'hourly-forecast-card__temp'}>{temp}°</span>
